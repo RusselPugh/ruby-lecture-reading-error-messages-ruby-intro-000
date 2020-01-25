bc1f73e4914f64125441a15c5d1b62577e5dc765
Russel Pugh <russ_scooby@yahoo.com>
2020-01-25 06:54:06 +0000

    Automatically backed up by Learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -4,7 +4,7 @@ describe "Not having any errors and being all green" do
     it 'raises a NameError when encountering undefined barewords' do
       expect{
         load './lib/a_name_error.rb'
-      }.to raise_error(SyntaxError)
+      }.to raise_error(NameError)
     end
   end
 
@@ -28,7 +28,7 @@ describe "Not having any errors and being all green" do
     it 'raises a ZeroDivisionError for dividing by zero' do
       expect{
         load './lib/a_division_by_zero_error.rb'
-      }.to raise_error(DivisionErrors)
+      }.to raise_error(ZeroDivisionError)
     end
   end
 end
